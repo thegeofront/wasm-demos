1dfbe141bef02ebfbe13c73fcdf106a7959f8433
jos <me@josfeenstra.nl>
2022-04-08 15:46:53 +0200

    discovered that .mjs es6 modules are supported

--- cpp-1-embind/build.sh
+++ cpp-1-embind/build.sh
@@ -1,6 +1,10 @@
+# original
+# emcc lerp.cpp \
+#   -lembind \
+#   -o lerp.js \
+#   -s MODULARIZE=1
+
 emcc lerp.cpp \
   -lembind \
-  -o lerp.js \
+  -o lerp.mjs \
   -s MODULARIZE=1
-
-#g++ clang++
